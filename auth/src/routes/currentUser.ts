1fedc89ea84e3684057b68b76e95a54a322c61bf
gabolque <gluquemiranda@gmail.com>
2022-06-02 15:37:42 -0500

    CurrentUser Route

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import jwt from "jsonwebtoken";
+import { JWT_KEY } from "../../config";
 
 const router = express.Router();
 
-router.get('/current-user', (req, res) => {
-  res.send("Hi there!");
+router.get('/api/users/current-user', (req, res) => {
+  if(!req.session?.jwt) return res.send({ currentUser: null });
+
+  try {
+    const payload = jwt.verify(req.session.jwt, JWT_KEY);
+    return res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
+
 });
 
 export { router as currentUserRouter };
